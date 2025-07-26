7dda3d097a15699cde91b5cf7bf72e78c56b2c1d
m1racle <anamoladhikar1998@gmail.com>
2025-07-25 23:49:24 -0500

    some notes and basic

--- hello.py
+++ hello.py
@@ -1,21 +1,65 @@
-print ("hello, world!") # yay! first python command. 
+# yay! first python command. 
+#
+
+# Ask user for their name and storre it in a variable called "name"
+name = input("What's your name?\n")
+
+# Print a greeting message to the user and the name they entered
+print("Hello "+ name + ", You are going to land your dream job in FAANG")
+print (name)
+
 
 """
-functions:
+Functions:
 
 It is either action or verb that lets you do something in the program. Any language has predetermined functions.
 
 One of the example is print() function. It prints the text inside the parentheses to the console.
 In this case, it prints "hello, world!" to the console.
 
-The next one is Arguments:
+The other one is input() function. It takes input from the user and returns it as a string.
+In this case, it takes the user's name and returns it as a string.
+
+
+Arguments:
 
 Arguments are the values you pass to a function. In this case, "hello, world!"
 Simply put, arguments input to function that somehow influences its behavior.
 
+Variables
+It is a way to store data in the program.
+
+In this case, it stores the user's name in the variable "name".
+here = is assignment operator, which assigns the value on the right to the variable on the left.
+
+here + is concatenation operator, which combines two strings into one.
+In this case, it combines "Hello " with the user's name and ", You are going to land your dream job in FAANG".
+
++ joins strings directly;
+, separates items with spaces.
+
 Side Effects:
 Side effects are changes that occur in the program as a result of executing a function.
 In this case, the side effect is that the text "hello, world!" appears in the console.
+
+Comments:
+Comments are lines in the code that are ignored by the Python interpreter. It is used to explain the code and notes.
+
+It can be also used to temporarily disable a line of code without deleting it like Pseudo code.
+
+In this case, the comment explains what the code does.
+Comments start with a hash symbol (#) and continue to the end of the line or can be a block of text enclosed in triple quotes. 
+
+
+Bugs:
+Error in the code that prevents it from running as expected.
+In this case, there are no bugs in the code, so it runs as expected and prints "hello, world!" to the console.
+
+But: if we miss the parentheses in the print function, like this:
+print ("hello, world!"
+we will get a syntax error, which is a type of bug that prevents the code from running.
+
+
 This is a simple example of how functions, arguments, and side effects work in Python.
 
 """
