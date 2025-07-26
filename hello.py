c19af3d59c91bf20d2a33b65097920ebae3be483
m1racle <anamoladhikar1998@gmail.com>
2025-07-25 23:09:43 -0500

    Basic Hello world with notes.

---
+++ hello.py
@@ -0,0 +1,21 @@
+print ("hello, world!") # yay! first python command. 
+
+"""
+functions:
+
+It is either action or verb that lets you do something in the program. Any language has predetermined functions.
+
+One of the example is print() function. It prints the text inside the parentheses to the console.
+In this case, it prints "hello, world!" to the console.
+
+The next one is Arguments:
+
+Arguments are the values you pass to a function. In this case, "hello, world!"
+Simply put, arguments input to function that somehow influences its behavior.
+
+Side Effects:
+Side effects are changes that occur in the program as a result of executing a function.
+In this case, the side effect is that the text "hello, world!" appears in the console.
+This is a simple example of how functions, arguments, and side effects work in Python.
+
+"""
